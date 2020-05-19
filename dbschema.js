dcefd413bb02431f9bff55026208dca9e5d895cc
Mayu Imanaka <imamayu.1215@gmail.com>
2020-05-19 09:44:42 -0700

    add:fbauth

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = [
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: "2020-05-18T20:00:38.507Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+]
--- functions/index.js
+++ functions/index.js
@@ -29,24 +29,59 @@ app.get('/screams', (req,res) =>{
             screams.push({
                 screamId: doc.id,
                 body: doc.data().body,
-                userHand: doc.data().userHandle,
-                createAt: doc.data().createdAt
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
             });
         });
         return res.json(screams);
     })
-    .catch((err) => console.error(err));
-});
+	.catch((err) => {
+        console.error(err);
+        res.status(500).json({ error: err.code });
+    });
+    });
 
-app.post('/screams', (req,res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found');
+        return res.status(403).json({ error:'Unauthorized' });
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then((decodedToken) => {
+            req.user = decodedToken;
+            console.log(decodedToken);
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then((data) => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch((err) => {
+            console.error('Error while verifying token ', err);
+            return res.status(403).json(err);
+        });
+};
+
+// Post one scream
+app.post('/scream', FBAuth, (req,res) => {
+    if(req.body.body.trim() === ''){
+        return res.status(400).json({ body: 'Body must not be empty' });
+    }
+
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
-    db
-    .collection('screams')
+    db.collection('screams')
     .add(newScream)
     .then((doc) => {
         res.json({ message: `document ${doc.id} created successfully` });
@@ -129,6 +164,40 @@ app.post('/signup', (req, res) => {
             return res.status(500).json({ error: err.code });
         }
     });
+});
+
+//login
+
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if(isEmpty(user.email)) errors.email = "Must not be empty";
+    if(isEmpty(user.password)) errors.password = "Must not be empty"
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+        .then((data) => {
+            return data.user.getIdToken();
+        })
+        .then((token) => {
+            return res.json({token});
+        })
+        .catch((err)=> {
+            console.error(err);
+            if(err.code === 'auth/wrong-password'){
+                return res
+                .status(403)
+                .json({ general: 'Wrong credentials, please try again' });
+            }else return res.status(500).json({ error:err.code });
+        });
 });
 
 exports.api = functions.https.onRequest(app);
