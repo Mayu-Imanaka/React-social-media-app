b36041dda468e9e2eb437ab1c4710c7387455f92
Mayu Imanaka <imamayu.1215@gmail.com>
2020-05-19 19:32:40 -0700

    update:divided file, add:userImg and userDetail

--- dbschema.js
+++ dbschema.js
@@ -1,9 +1,21 @@
 let db = [
+    users: [
+        {
+            userId: '7QCogqeB8rYcYeyETXoGteS4T6A2',
+            email: 'new5@email.com',
+            handle: 'new5',
+            createdAt: '2020-05-20T01:12:25.441Z',
+            imageUrl: 'https://firebasestorage.googleapis.com/v0/b/socialape200515.appspot.com/o/84628627725.jpg?alt=media',
+            bio: 'Helloo, my name is hill man, nice to meet you',
+            website: 'https://hillman.com',
+            location: 'Vancouver, Canada'
+        }
+    ],
     screams: [
         {
             userHandle: 'user',
             body: 'this is the scream body',
-            createdAt: "2020-05-18T20:00:38.507Z",
+            createdAt: "2020-05-20T01:12:25.441Z",
             likeCount: 5,
             commentCount: 2
         }
